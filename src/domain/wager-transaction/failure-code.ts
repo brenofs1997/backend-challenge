f9d91bd2ce4c15b42ac1e785a966448c45a9d43e
Breno Fernandes <brenofs1997@gmail.com>
2026-08-31 19:01:19 -0300

    feat: implement FailureCode

---
+++ src/domain/wager-transaction/failure-code.ts
@@ -0,0 +1,11 @@
+export enum FailureCode {
+  InsufficientFunds = 'INSUFFICIENT_FUNDS', 
+  ReversalWouldOverdraw = 'REVERSAL_WOULD_OVERDRAW', 
+  ReferenceNotFound = 'REFERENCE_NOT_FOUND', 
+  ReferenceMismatch = 'REFERENCE_MISMATCH',   
+  ReferenceAlreadyReversed = 'REFERENCE_ALREADY_REVERSED', 
+  ReversalAmountMismatch = 'REVERSAL_AMOUNT_MISMATCH', 
+  CurrencyMismatch = 'CURRENCY_MISMATCH',
+  IdempotencyConflict = 'IDEMPOTENCY_CONFLICT',
+  InvalidPayload = 'INVALID_PAYLOAD',
+}
